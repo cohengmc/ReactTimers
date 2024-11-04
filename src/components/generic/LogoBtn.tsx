5d09a3da3974d88d0a0185ad97bc3a3f34dd694d
cohengmc <34006245+cohengmc@users.noreply.github.com>
2024-11-04 17:40:02 -0500

    trying to fix image bug

--- src/components/generic/LogoBtn.tsx
+++ src/components/generic/LogoBtn.tsx
@@ -8,17 +8,17 @@ const LogoBtn = ({ name, onClick }: BtnProps) => {
         <div>
             <button onClick={onClick} className="btn">
                 {name === 'play' ? (
-                    <img src={'/images/play-svgrepo-com.svg'} alt="play logo" className="btnLogo" />
+                    <img src={'/assignment-1-cohengmc/images/play-svgrepo-com.svg'} alt="play logo" className="btnLogo" />
                 ) : name === 'pause' ? (
-                    <img src={'/images/pause-alt-svgrepo-com.svg'} alt="pause logo" className="btnLogo" />
+                    <img src={'/assignment-1-cohengmc/images/pause-alt-svgrepo-com.svg'} alt="pause logo" className="btnLogo" />
                 ) : name === 'reset' ? (
-                    <img src="/images/reset-svgrepo-com.svg" alt="reset logo" className="btnLogo" />
+                    <img src="/assignment-1-cohengmc/images/reset-svgrepo-com.svg" alt="reset logo" className="btnLogo" />
                 ) : name === 'back' ? (
-                    <img src="/images/left-arrow-svgrepo-com.svg" alt="back logo" className="btnLogo" />
+                    <img src="/assignment-1-cohengmc/images/left-arrow-svgrepo-com.svg" alt="back logo" className="btnLogo" />
                 ) : name === 'ff' ? (
-                    <img src="/images/fast-forward-svgrepo-com.svg" alt="ff logo" className="btnLogo" />
+                    <img src="/assignment-1-cohengmc/images/fast-forward-svgrepo-com.svg" alt="ff logo" className="btnLogo" />
                 ) : (
-                    <img src="/images/alert-error-svgrepo-com.svg" alt="reset logo" className="btnLogo" />
+                    <img src="/assignment-1-cohengmc/images/alert-error-svgrepo-com.svg" alt="reset logo" className="btnLogo" />
                 )}
             </button>
         </div>
