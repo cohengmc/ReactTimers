7502bc2966fd5a02661f215440a4552d13fc56c4
cohengmc <34006245+cohengmc@users.noreply.github.com>
2024-11-04 17:30:53 -0500

    moving images

--- src/components/generic/LogoBtn.tsx
+++ src/components/generic/LogoBtn.tsx
@@ -8,17 +8,17 @@ const LogoBtn = ({ name, onClick }: BtnProps) => {
         <div>
             <button onClick={onClick} className="btn">
                 {name === 'play' ? (
-                    <img src={'src/images/play-svgrepo-com.svg'} alt="play logo" className="btnLogo" />
+                    <img src={'/images/play-svgrepo-com.svg'} alt="play logo" className="btnLogo" />
                 ) : name === 'pause' ? (
-                    <img src={'src/images/pause-alt-svgrepo-com.svg'} alt="pause logo" className="btnLogo" />
+                    <img src={'/images/pause-alt-svgrepo-com.svg'} alt="pause logo" className="btnLogo" />
                 ) : name === 'reset' ? (
-                    <img src="src/images/reset-svgrepo-com.svg" alt="reset logo" className="btnLogo" />
+                    <img src="/images/reset-svgrepo-com.svg" alt="reset logo" className="btnLogo" />
                 ) : name === 'back' ? (
-                    <img src="src/images/left-arrow-svgrepo-com.svg" alt="back logo" className="btnLogo" />
+                    <img src="/images/left-arrow-svgrepo-com.svg" alt="back logo" className="btnLogo" />
                 ) : name === 'ff' ? (
-                    <img src="src/images/fast-forward-svgrepo-com.svg" alt="ff logo" className="btnLogo" />
+                    <img src="/images/fast-forward-svgrepo-com.svg" alt="ff logo" className="btnLogo" />
                 ) : (
-                    <img src="src/images/alert-error-svgrepo-com.svg" alt="reset logo" className="btnLogo" />
+                    <img src="/images/alert-error-svgrepo-com.svg" alt="reset logo" className="btnLogo" />
                 )}
             </button>
         </div>
